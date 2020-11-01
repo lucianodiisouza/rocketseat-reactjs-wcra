77e1c406a6874d4facdc4fc0521df3c25de34c74
lucianodiisouza <lucianodiisouza@hotmail.com>
2020-11-01 00:22:07 -0300

    Header component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import React from "react";
 import Header from "./components/Header";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header title="ReactJs" />
+      <Header title="NodeJS" />
+    </>
+  );
 }
 
 export default App;
